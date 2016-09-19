0b37674aa1b931d993ed2bba469a1651e2ed3f24
eNiize <alegun93@gmail.com>
2016-09-19 15:24:54 +0200

    Search list
    Started on the index search list
    Changed some .js file names
    Changed some .css file names
    Added searchTable.css

--- scripts/guiController.js
+++ scripts/guiController.js
@@ -11,7 +11,7 @@ function closeNav() {
 /********** /Sidenav push **********/
 
 /********** Calendar list search **********/
-function myFunction() {
+function calendarList() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -33,3 +33,25 @@ function myFunction() {
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+function searchList() {
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
