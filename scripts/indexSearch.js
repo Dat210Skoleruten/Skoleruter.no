330478edcdc1e3dd168b2474e3d3d5d320035d95
RabaGhast <fabianboe@hotmail.com>
2016-09-26 15:59:39 +0200

    Minor fix on same

--- scripts/indexSearch.js
+++ scripts/indexSearch.js
@@ -36,13 +36,11 @@ function getIndexListItems() {
     var schoolArray = getSchoolArray();
     $("#indexList").children().empty();
     $.each(schoolArray, function (index, value) {
-        var elem1 = $("<tr></tr>");
-        var elem2 = $("<td></td>");
-        var elem3 = $("<a href='calendar.html'>" + value.Skolenavn + "</a>");
-        elem3.click(function(){
+        var elem1 = $("<tr><td></td></tr>");
+        var elem2 = $("<a href='calendar.html'>" + value.Skolenavn + "</a>");
+        elem2.click(function(){
             Cookies.set("selected", $(this).text());
         });
-        elem2.append(elem3);
         elem1.append(elem2);
         $("#indexList").append(elem1);
     });
@@ -55,10 +53,12 @@ function getIndexListItemsPos() {
     $("#indexList").children().empty();
     var fiveClosest = findClosest();
     for(var i = 0; i < 5; i++){ //5 closest schools
-        var element = $("<tr><td><a href='calendar.html'>" + fiveClosest[i][1] + "</a></td></tr>");
-        element.click(function(){
-            Cookies.set("selected", $(this).val());
+        var elem1 = $("<tr><td></td></tr>");
+        var elem2 = $("<a href='calendar.html'>" + fiveClosest[i][1] + "</a>");
+        elem2.click(function(){
+            Cookies.set("selected", $(this).text());
         });
-        $("#indexList").append(element);
+        elem1.append(elem2);
+        $("#indexList").append(elem1);
     }
 }
