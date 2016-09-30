d6689244ed26e4dbbd5cf9c752a83fb417983d45
thrasmussen <thrasmussen@outlook.com>
2016-09-30 10:37:04 +0200

    Commented on "skoleruter" Denne vil slutte å fungere aug.2017

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -8,7 +8,7 @@ The function getSchoolData combines these two datasets to one containing all sch
 
 if (Session.get("schoolRoutes") == null){
   console.log("schoolRoutes is null");
-  Papa.parse("http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv", {
+  Papa.parse("http://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/32d52130-ce7c-4282-9d37-3c68c7cdba92/download/skolerute-2016-17.csv", { //Denne linken stopper å fungere August 2017
     download: true,
     header: true,
   	complete: function(results) {
