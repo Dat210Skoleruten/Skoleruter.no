c2574743617b08a16cee8acbeadd2b60cdc8353f
RabaGhast <fabianboe@hotmail.com>
2016-10-04 12:21:02 +0200

    moved printMySchools() to mySchools.js

--- scripts/mySchools.js
+++ scripts/mySchools.js
@@ -63,6 +63,29 @@ function checkCalSelect(){
   }
 }
 
+function printMySchools(){
+  var elem = document.getElementById("myFavScho");
+
+  if(Cookies.get("mySchools") != null){
+    var theString = "Skoler valgt: "
+    var arr = Cookies.get("mySchools").split(",");
+    for(var i = 0; i < arr.length; i++){
+      if(i == arr.length -1){
+          theString += arr[i] + ".";
+      }
+      else{
+        theString += arr[i] + ", ";
+      }
+    }
+    elem.innerHTML = theString;
+  }
+  else{
+    elem.innerHTML = "Ingen skoler valgt!";
+    elem.style.color = "red";
+  }
+}
+
+
 function calenderCookie(){
   var elem = document.getElementById("selecookiefav");
   var schoStr = "";
