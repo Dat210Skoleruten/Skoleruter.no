df8f26860aec0223328c292ccec0a9e51d6593be
RabaGhast <fabianboe@hotmail.com>
2016-10-04 09:50:42 +0200

    ditto fix

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -242,7 +242,7 @@ class Calendar {
             }
 
 
-            if (this.schools[skoler].Datoer[dates][1] != "Søndag" && this.schools[skoler].Datoer[dates][1] != "Lørdag" && this.schools[skoler].Datoer[dates][1] != null) {
+            if (this.schools[skoler].Datoer[dates][1] != "Søndag" && this.schools[skoler].Datoer[dates][1] != "Lørdag" && this.schools[skoler].Datoer[dates][1] != "") {
               list = $("<li><a><span class='dateNum'>" + dates.substring(8, 10) + "</span> " + this.schools[skoler].Datoer[dates][1] + "</a></li>");
               $("#myUL").append(list);
             };
