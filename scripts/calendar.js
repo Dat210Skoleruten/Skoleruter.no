bdebbc9ed49b86cdf3e6cd39a2935aa39e2ec49e
eNiize <alegun93@gmail.com>
2016-10-06 10:23:21 +0200

    Removed search in  calendar list

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -39,23 +39,6 @@ $(document).ready(function() {
   });
 });
 
-function calendarList() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
-
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
-
 // findSchool finds the school with the name as string from array
 function findSchool(str, array) {
   var tmpArr = [];
