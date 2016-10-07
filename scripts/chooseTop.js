5ac2abe038e2d23ce313ba22392e2a0d219f4b3f
Krisbo <kristoffer_boe@hotmail.com>
2016-10-07 13:51:15 +0200

    change of function that finds top element in visible list
    Changed function so that we can search in list and press enter to choose
    the school at the top of the list.

--- scripts/chooseTop.js
+++ scripts/chooseTop.js
@@ -1,41 +1,26 @@
+var schoolText = "";
 
 $(document).keydown(function (e) {
   var theTable = document.getElementById("indexList");
-  if(theTable.firstChild != null && theTable.style.display != "none"){
-    for(var i = 0; i < theTable.firstChild.getElementsByTagName('a').length/2; i++){
-      if(theTable.getElementsByTagName('tr')[i].style.display != "none"){
-        console.log(theTable.firstChild.getElementsByTagName('a')[i].innerHTML);
-        i++;
-      }
-      
+
+  for(var i = 0; i < theTable.getElementsByTagName('tr').length; i++){
+    if(theTable.style.display == "none"){
+      break;
+    }else if(theTable.getElementsByTagName('tr')[i].style.display == "none"){
+    }else{
+      console.log("Found right school");
+      schoolText = theTable.getElementsByTagName('tr')[i].firstChild.firstChild.firstChild.innerHTML;
+      //changeAutoText(schoolText);
+      break;
     }
   }
-
-
   if (e.keyCode == 13 && theTable.firstChild != null) {
-    console.log("Enter pressed!")
-
-    // console.log(theTable.rows.length)
-    console.log(theTable.firstChild.getElementsByTagName('a')[i].innerHTML); //PRINTS EIGANES SKOLE
-    for(var i = 0; i < theTable.getElementsByTagName('tr').length; i++){
-      console.log("Entered the for-loop!");
-      console.log(theTable.firstChild.getElementsByTagName('a')[i].innerHTML);
-      //trTable[i].innerHTML = "SATTAN";
-      //console.log(trTable[i].innerHTML);
-      if(theTable.style.display == "none"){
-        break;
-      }
-      else if(theTable.getElementsByTagName('tr')[i].style.display == "none"){
-        console.log("HELLO THERE YOU SHIT!");
-        i++;
-      }else{
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        Cookies.set("selected", theTable.firstChild.getElementsByTagName('a')[i].innerHTML);
-        window.location.href = "calendar.html";
-        break;
-      }
-
-    }
-
+    e.preventDefault();
+    Cookies.set("selected", schoolText);
+    window.location.href = "calendar.html";
   }
 });
+
+function changeAutoText(schoolText){
+  //document.getElementById('indexSearch');
+}
