334be876e840f6e462811a1ffce59dc17fe1dca7
eNiize <alegun93@gmail.com>
2016-10-10 12:21:52 +0200

    changed legendbar css in calendar.css too legendBar.css

--- scripts/printmySchools.js
+++ scripts/printmySchools.js
@@ -6,7 +6,7 @@ function printMySchools() {
         var arr = Cookies.get("mySchools").split(",");
 
         for (var i = 0; i < arr.length; i++) {
-                theString += "<p class='favorite'>" + arr[i] + "<span class='glyphicon glyphicon-remove legendRemove' aria-hidden='true'></span></p>";
+                theString += "<p class='favorite'>" + arr[i] + "<span class='glyphicon glyphicon-remove legendRemove' onclick='calendarCookie()' aria-hidden='true'></span></p>";
         }
         console.log(theString);
         elem.innerHTML = theString;
