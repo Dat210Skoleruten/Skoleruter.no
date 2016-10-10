d5b10b8826da2f4f174d90f8c6227884b70de81b
Alexander Gundersen <alegun93@gmail.com>
2016-10-10 18:08:07 +0200

    Cleanup
    Deleted a lot of unused stylesheet and javascript code in <head> for
    each html file
    
    Cleaned up unused and commented code for IndexSearch

--- scripts/indexSearch.js
+++ scripts/indexSearch.js
@@ -85,29 +85,6 @@ function getIndexListItems() {
     $("#indexList").hide();
 }
 
-/*
- function getIndexListItemsPos(closest) {
- console.log("getIndexListItemsPos: ", closest)
- $("#indexList").show();
- $("#indexList").children().empty();
- var fiveClosest = closest;
- for (var i = 0; i < 5; i++) { //5 closest schools
- var elem1 = $("<tr></tr>");
- var elem2 = $("<td></td>");
- var elem3 = $("<div></div>");
- var elem4 = $("<a href='calendar.html' class='listElement'>" + fiveClosest[i][1] + "</a>");
- var elem5 = $("<td><div><a href='#' class='addButton'><span class='glyphicon glyphicon-star-empty' aria-hidden='true'></span></a></div></td>");
- elem2.click(function () {
- Cookies.set("selected", $(this).text());
- });
- elem3.append(elem4);
- elem2.append(elem3);
- elem1.append(elem2);
- elem1.append(elem5);
- $("#indexList").append(elem1);
- }
- }*/
-
 function getIndexListItemsPos(closest) {
     //console.log("getIndexListItemsPos: ", closest)
     var ArrayWSchools = [];
@@ -151,6 +128,5 @@ function getIndexListItemsPos(closest) {
         elem5.append(elem6);
         elem1.append(elem5);
         $("#indexList").append(elem1);
-
     });
 }
