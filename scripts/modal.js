3472c58380a76ce0d85217d90a91c62e0640ba37
Alexander Gundersen <alegun93@gmail.com>
2016-10-31 11:19:29 +0100

    Helpsenter on all pages / removed helpicon in searchbar

--- scripts/modal.js
+++ scripts/modal.js
@@ -12,20 +12,21 @@ $(document).ready(function () {
     // When the user clicks on the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
-    }
+    };
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
-    }
+    };
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    }
+    };
 
+    /* Index.html */
     $("#gif1").click(function(){
         if($(this).attr("src") ==  "img/play.png"){
             $(this).attr("src", "img/Soke_og_Bruke_kalender.gif");
@@ -47,6 +48,31 @@ $(document).ready(function () {
             $(this).attr("src", "img/GPS.gif");
         }else{
             $(this) .attr("src", "img/play.png");
+        }
+    });
+
+    /* Helpsenter in other html files than index.html */
+    $("#gif4").click(function(){
+        if($(this).attr("src") ==  "../img/play.png"){
+            $(this).attr("src", "../img/Soke_og_Bruke_kalender.gif");
+        }else{
+            $(this).attr("src", "../img/play.png");
+        }
+    });
+
+    $("#gif5").click(function(){
+        if($(this).attr("src") ==  "../img/play.png"){
+            $(this).attr("src", "../img/LeggeTil_Fjerne_MineSkoler.gif");
+        }else{
+            $(this).attr("src", "../img/play.png");
+        }
+    });
+
+    $("#gif6").click(function(){
+        if($(this).attr("src") ==  "../img/play.png"){
+            $(this).attr("src", "../img/GPS.gif");
+        }else{
+            $(this) .attr("src", "../img/play.png");
         }
     });
 });
