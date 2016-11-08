ad60d3117d84be927ccd451c70868ed0da15bb6b
Alexander Gundersen <alegun93@gmail.com>
2016-11-08 13:17:05 +0100

    Removed unused stylesheets, added hidingThings.js to other pages

--- scripts/hidingThings.js
+++ scripts/hidingThings.js
@@ -1,17 +1,15 @@
-$(document).mouseup(function (e)
-{
+$(document).mouseup(function (e) {
     var indexlist = $("#indexList");
     var navbar = $("#mySidenav")
 
     if (!indexlist.is(e.target) // if the target of the click isn't the container...
-        && indexlist.has(e.target).length === 0) // ... nor a descendant of the container
-    {
+        && indexlist.has(e.target).length === 0) { // ... nor a descendant of the container
         indexlist.hide();
         $("#indexSearch").val("");
     }
+
     if (!navbar.is(e.target) // if the target of the click isn't the container...
-        && navbar.has(e.target).length === 0) // ... nor a descendant of the container
-    {
+        && navbar.has(e.target).length === 0) { // ... nor a descendant of the container
         closeNav();
     }
 });
