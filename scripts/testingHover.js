3645f612f8c8a8f2f182e1060a87b48bb8e1c812
Krisbo <kristoffer_boe@hotmail.com>
2016-11-08 17:48:42 +0100

    test of hoverlimitation

--- scripts/testingHover.js
+++ scripts/testingHover.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-    $("#indexList td a").mouseenter(function(){
-        $("#indexList td a").css("color", "#fff");
-        $("#indexList td a").css("background-color", "#495156")
+function testHover(){
+    $("#indexList td a").mouseover(function(){
+        $(this).css("color", "#fff");
+        $(this).css("background-color", "#495156")
     });
-    $(".addbutton td a").mouseenter(function(){
-        $("#indexList td a").css("color", "#fff");
-        $("#indexList td a").css("background-color", "#495156")
+    $(".addbutton td a").mouseover(function(){
+        $(this).css("color", "#fff");
+        $(this).css("background-color", "#495156")
     });
     
-    $(".addbutton td a").mouseleave(function(){
-        $("#indexList td a").css("color", "#495156");
-        $("#indexList td a").css("background-color", "#e5e5e5")
+    $("#indexList td a").mouseleave(function(){
+        $(this).css("color", "#495156");
+        $(this).css("background-color", "#e5e5e5")
     });
     $(".addbutton td a").mouseleave(function(){
-        $("#indexList td a").css("color", "#495156");
-        $("#indexList td a").css("background-color", "#e5e5e5")
+        $(this).css("color", "#495156");
+        $(this).css("background-color", "#e5e5e5")
     });
-});
+}
+    
