dd95ec3773f99dcbfa07cf5a28394a30e0ca57da
Krisbo <kristoffer_boe@hotmail.com>
2016-11-10 09:02:14 +0100

    test

--- scripts/hidingThings.js
+++ scripts/hidingThings.js
@@ -1,32 +1,5 @@
-if(!checkIfMobile()){
-    $(document).mouseup(function (e) {
-    var indexlist = $("#indexList");
-    var navbar = $("#mySidenav")
 
-    if (!indexlist.is(e.target) // if the target of the click isn't the container...
-        && indexlist.has(e.target).length === 0) { // ... nor a descendant of the container
-        indexlist.hide();
-        $("#indexSearch").val("");
-    }
-
-    if (!navbar.is(e.target) // if the target of the click isn't the container...
-        && navbar.has(e.target).length === 0) { // ... nor a descendant of the container
-        closeNav();
-    }
-    });
-}
-if(checkIfMobile()){
-    $(document).mouseup(function (e) {
-    var navbar = $("#mySidenav")
-
-    if (!navbar.is(e.target) // if the target of the click isn't the container...
-        && navbar.has(e.target).length === 0) { // ... nor a descendant of the container
-        closeNav();
-    }
-    });
-}
-    
-function checkIfMobile(){
+function isMobile(){
     if( navigator.userAgent.match(/Android/i)
         || navigator.userAgent.match(/webOS/i)
         || navigator.userAgent.match(/iPhone/i)
