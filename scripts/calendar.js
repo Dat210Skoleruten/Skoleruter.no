b6c4c3744e4c67a2baec479b9e56c582c0e00923
RabaGhast <fabianboe@hotmail.com>
2016-11-14 11:44:20 +0100

    ditto redefining

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -119,9 +119,9 @@ function swipedetect(el, callback){
     startY,
     distX,
     distY,
-    threshold = 150, //required min distance traveled to be considered swipe
+    threshold = 100, //required min distance traveled to be considered swipe
     restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
+    allowedTime = 500, // maximum time allowed to travel that distance
     elapsedTime,
     startTime,
     handleswipe = callback || function(swipedir){}
