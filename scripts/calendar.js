c7a7dc3a64e7673812e5d8cba657f865551cf111
RabaGhast <fabianboe@hotmail.com>
2016-12-02 13:31:06 +0100

    debugging

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -15,7 +15,9 @@ $(document).ready(function () {
 
     parseData(function () {
         setSchoolData(Cookies.get('selected'), Cookies.get("calendarType")); // Sets name & hyperlink on html document
+        console.log("before");
         cal = new calendar(Cookies.get(Cookies.get("calendarType")), getSchoolData());
+        console.log("after");
         cal.buildCalendar(); // Builds calendar
         cal.buildList(); // Builds List
         cal.addHover();
