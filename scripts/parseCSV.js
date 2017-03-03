df5f86245bb10cf7e1af5b477e0ef5a81d88e1ed
RabaGhast <fabianboe@hotmail.com>
2017-03-03 10:41:16 +0100

    Testing trondheim data set

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -11,6 +11,7 @@ schoolPaths = []; // schoolPaths["skolenavn slik som på subdomenet"] = ["skoler
 schoolPaths["stavanger"] = ["https://open.stavanger.kommune.no/dataset/86d3fe44-111e-4d82-be5a-67a9dbfbfcbb/resource/21cfc45a-d2bf-448a-a883-210ee4a96d9a/download/skolerute.csv", "https://open.stavanger.kommune.no/dataset/8f8ac030-0d03-46e2-8eb7-844ee11a6203/resource/8d13aca1-a3b3-49d5-8728-8dc310ef9f4a/download/skoler.csv"];
 schoolPaths["gjesdal"] = ["skolerute_gjesdal.csv", "skoler_gjesdal.csv"];
 schoolPaths["baerum"] = ["https://open.stavanger.kommune.no/dataset/6837c1de-6dce-48a3-a8a6-e59630912779/resource/19f6c237-bc56-4c1d-bb59-4538a3215eba/download/skolerute-2016-17.csv", "https://open.stavanger.kommune.no/dataset/4a5f420f-453d-4e23-85f5-0b1d5d4a1fe0/resource/95bc274b-04bc-4a45-82ce-3d22ef46225d/download/skoler-i-baerum.csv"];
+schoolPaths["trondheim"] = ["https://open.stavanger.kommune.no/dataset/7f6df84e-409c-4509-ba95-23a13d0a6730/resource/1fae9af6-6960-4012-bba9-f68a20f6adf1/download/skoleruta-2017-2018.csv", "https://open.stavanger.kommune.no/dataset/055880c9-cb7e-4919-ab9f-e6d6ee096346/resource/70148039-78b7-43e5-b1d1-ee779971f65b/download/skolertrondheim.csv"];
 
 
 console.log("page is:", location.hostname);
@@ -48,7 +49,7 @@ function parseData(callback) {
         url = schoolPaths[Session.get("SelectedSet")][0];
         console.log(url);
         var start = new Date().getTime();
-        console.time("Skoleruter")
+        console.time("Skoleruter");
         Papa.parse(url, { 
             download: true,
             header: true,
@@ -67,7 +68,7 @@ function parseSecondData(callback) {
     if (Session.get("schools") == null) {
         url = schoolPaths[Session.get("SelectedSet")][1];
 
-        console.time("Skoler")
+        console.time("Skoler");
         Papa.parse(url, { 
             download: true,
             header: true,
@@ -97,6 +98,7 @@ function UrlExists(url)
 }
 
 function getSchoolData() {
+    console.time("getSchoolData");
     var schoolRoutes = Session.get("schoolRoutes");
     var schools = Session.get("schools");
     var data = [];
@@ -107,7 +109,7 @@ function getSchoolData() {
         if (data.length > 0) { // if data already has schools added to it
             var found = false;
             for (var j = 0; j < data.length; j++) {
-                if (data[j].name == entry.skole) { // if the school name of the current object in data is the same as the current entry from ajax
+                if (data[j].name.toLowerCase() == entry.skole.toLowerCase()) { // if the school name of the current object in data is the same as the current entry from ajax
                     found = true;
                     data[j].dates[entry.dato] = formatDato(entry); //adds date to data.dates array with the formatDato format
                 }
@@ -127,7 +129,7 @@ function getSchoolData() {
             schools[i]['Datoer'] = [];
             for (var j = 0; j < data.length; j++) {
 
-                if (schools[i].Skolenavn == data[j].name) {
+                if (schools[i].Skolenavn.toLowerCase() == data[j].name.toLowerCase()) {
 
                     schools[i].Datoer = data[j].dates;
                     continue loop1;
@@ -136,6 +138,7 @@ function getSchoolData() {
             schools.splice(i, 1);
             i--;
         }
+    console.timeEnd("getSchoolData");
     return schools;
 }
 
