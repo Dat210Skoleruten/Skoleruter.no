4600e48a379d7e06e9952237c5de85d011a6fe31
Alexander Gundersen <alegun93@gmail.com>
2017-03-07 17:47:31 +0100

    hjelpsenter accordion latterlig mye mer sexy

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,8 +2,15 @@
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function () {
+    acc[i].onclick = function() {
         this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
     }
 }
+
+
