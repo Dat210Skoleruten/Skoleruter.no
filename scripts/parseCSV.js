b731090d5de3ef52a13c48e4553a79f12d4473d4
RabaGhast <fabianboe@hotmail.com>
2017-03-08 13:54:00 +0100

    fixed error in getCSV

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -13,8 +13,7 @@ schoolPaths["gjesdal"] = ["skolerute_gjesdal.csv", "skoler_gjesdal.csv"];
 schoolPaths["baerum"] = ["https://open.stavanger.kommune.no/dataset/6837c1de-6dce-48a3-a8a6-e59630912779/resource/19f6c237-bc56-4c1d-bb59-4538a3215eba/download/skolerute-2016-17.csv", "https://open.stavanger.kommune.no/dataset/4a5f420f-453d-4e23-85f5-0b1d5d4a1fe0/resource/95bc274b-04bc-4a45-82ce-3d22ef46225d/download/skoler-i-baerum.csv"];
 schoolPaths["trondheim"] = ["https://open.stavanger.kommune.no/dataset/7f6df84e-409c-4509-ba95-23a13d0a6730/resource/f9f73bc7-49ce-442d-92c2-3aa03c577451/download/skoleruta-2016-2017-trondheim-kommune.csv", "https://open.stavanger.kommune.no/dataset/055880c9-cb7e-4919-ab9f-e6d6ee096346/resource/70148039-78b7-43e5-b1d1-ee779971f65b/download/skolertrondheim.csv"]; //["https://open.stavanger.kommune.no/dataset/7f6df84e-409c-4509-ba95-23a13d0a6730/resource/1fae9af6-6960-4012-bba9-f68a20f6adf1/download/skoleruta-2017-2018.csv", "https://open.stavanger.kommune.no/dataset/055880c9-cb7e-4919-ab9f-e6d6ee096346/resource/70148039-78b7-43e5-b1d1-ee779971f65b/download/skolertrondheim.csv"];
 
-var node = require("../Node/app.js");
-console.log(node.getKommuneData("gjesdal"));
+console.log(getLocalData_Array_fromTreatedCSV("gjesdal"));
 
 if (location.hostname.split('.')[0] == "dev" || location.hostname.split('.')[0] == "skoleruter") {
     window.location.href = "/kommune.html";
@@ -37,7 +36,7 @@ if (Cookies.get("mySchools") != null && Cookies.get("visiting") == null) {
 Cookies.set("visiting", "true");
 
 //######################################################################################
-
+/*
 function parseData(callback) {
     if (Session.get("schoolRoutes") == null) {
         url = schoolPaths[Session.get("SelectedSet")][0];
@@ -81,6 +80,40 @@ function parseSecondData(callback) {
         callback();
     }
 }
+*/
+
+
+function getLocalData_Array_fromTreatedCSV(name) {
+    parsedData = Papa.parse('../Node/CSV/' + name + ".csv", {
+        download: true,
+        header: true,
+        complete: function (results) {
+            return results;
+        }
+    }).data;
+    mergedData = [];
+    for (var i in parsedData) {
+
+        mergedData[i] = {
+            Skolenavn: parsedData[i].Skolenavn,
+            Latitude: parsedData[i].Latitude,
+            Longitude: parsedData[i].Longitude,
+            Hjemmeside: parsedData[i].Hjemmeside,
+            Datoer: formatDatoerFromCSV(parsedData[i].Datoer)
+        }
+    }
+    return mergedData;
+}
+
+function formatDatoerFromCSV(str){
+    arr = []
+    var lines = str.split("\[(.*?)\]");
+    for(var i in lines){
+        var parts = lines[i].split(";");
+        arr[parts[0]] = [parts[1], parts[2]];
+    }
+    return arr;
+}
 
 function getSchoolData() {
     console.time("getSchoolData");
@@ -105,29 +138,29 @@ function getSchoolData() {
                 //}
             }
             if (!found) { //if school is not already in data array, add school and current entry's date
-                data.push({name: entry.skole, dates: []});
+                data.push({ name: entry.skole, dates: [] });
                 data[data.length - 1].dates[entry.dato] = formatDato(entry);
             }
         } else {
-            data.push({name: entry.skole, dates: []});
+            data.push({ name: entry.skole, dates: [] });
             data[data.length - 1].dates[entry.dato] = formatDato(entry);
         }
     }
 
     loop1:
-        for (var i = 0; i < schools.length; i++) {
-            schools[i]['Datoer'] = [];
-            for (var j = 0; j < data.length; j++) {
+    for (var i = 0; i < schools.length; i++) {
+        schools[i]['Datoer'] = [];
+        for (var j = 0; j < data.length; j++) {
 
-                if (schools[i].Skolenavn == data[j].name) {
+            if (schools[i].Skolenavn == data[j].name) {
 
-                    schools[i].Datoer = data[j].dates;
-                    continue loop1;
-                }
+                schools[i].Datoer = data[j].dates;
+                continue loop1;
             }
-            schools.splice(i, 1);
-            i--;
         }
+        schools.splice(i, 1);
+        i--;
+    }
     console.timeEnd("getSchoolData");
     return schools;
 }
