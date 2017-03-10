69d8e8866241daf143f0877d66055a2f6d9b8dc3
RabaGhast <fabianboe@hotmail.com>
2017-03-10 22:24:21 +0100

    ditto

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -83,9 +83,26 @@ function parseSecondData(callback) {
 */
 
 function parseData(name, callback) {
-    var dataFromJSON = require("Node/JSON/" + name + ".JSON");
-    Session.set("schoolRoutes", dataFromJSON);
-    callback();
+    loadJSON(name, function (response) {
+        // Parse JSON string into object
+        var dataFromJSON = response;
+        Session.set("schoolRoutes", dataFromJSON);
+        callback();
+    });
+}
+
+function loadJSON(name, cb) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', "Node/JSON/" + name + ".JSON", true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            cb(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
 
 function getLocalData_Array_fromTreatedCSV(name) {
