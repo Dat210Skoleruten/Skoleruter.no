fc386418806a9251092b22ce1a99ff1410385773
RabaGhast <fabianboe@hotmail.com>
2017-03-11 11:24:06 +0100

    testing new JSON path

--- scripts/parseCSV.js
+++ scripts/parseCSV.js
@@ -95,7 +95,7 @@ function loadJSON(name, cb) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', "Node/JSON/" + name + ".JSON", true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', name + ".JSON", true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
